022dcf752b1e25614eaf56adc5c37061d7004b38
dreamjz <25699818+dreamjz@users.noreply.github.com>
2024-06-08 05:59:08 +0800

    project init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+)
+
+var img *ebiten.Image
+
+const (
+	screenWidth = 320
+	screeHeight = 240
+	frameWidth  = 19
+	frameHeight = 17
+	catRunCount = 4
+	frame0X     = 0
+	frame0Y     = 0
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
